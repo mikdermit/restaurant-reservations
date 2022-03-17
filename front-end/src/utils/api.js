7fe302b7d2d7a20708c0078688adebf19bf33903
Brick McDermott <82049931+mikdermit@users.noreply.github.com>
2022-03-17 16:03:46 -0400

    changing url

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,9 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const { API_BASE_URL = "https://restaurant-server-bm.herokuapp.com" } = process.env
+// const { API_BASE_URL = "https://restaurant-server-bm.herokuapp.com" } = process.env
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
